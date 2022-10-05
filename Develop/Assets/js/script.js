d2a1ef58b736996d483c46b15f8440b36a45c3c8
Richard Hong <rhong24@gmail.com>
2022-10-05 14:51:09 -0500

    adding in assets within develop folder, removed bootstrap rows from index.html for the most part, added in script.js which dynamically adds the bootstrap columns for each hour time slot and logic for handling coloring of past, present, future time events as well as saving and reading localStorage tasks respective of each time slot

---
+++ Develop/Assets/js/script.js
@@ -0,0 +1,85 @@
+var timeBlockContainer = $('.row-slots')
+var currentTimeHour = parseInt(moment().format('HH'))
+var timeSlots = $('.container')
+var locallyStoredDays = JSON.parse(localStorage.getItem("dayTasks"))
+
+var hourColumnContainer = $('<div>').attr('class', 'col-2')
+var hourCard = $('<a>').addClass('card h-100 p-3 my-3 hour')
+var textColumnContainer = $('<div>').attr('class', 'col-8')
+var textCard = $('<a>').addClass('card h-100 p-3 my-3 text')
+var textInput = $('<input>').attr({ type: 'text', class: 'textarea', placeholder: 'Enter task here (for multiple tasks seperate with a comma)', name: 'task-input'})
+var saveButtonColumnContainer = $('<div>').attr('class', 'col-2')
+var saveButtonCard = $('<button>').attr({class: 'card h-100 p-3 my-3 saveBtn', id: 'saveBtn'}).text('Save')
+var slotContainer = $('<div>').attr({ class: 'row no-gutters my-4'})
+
+timeBlockContainer.append(slotContainer)
+slotContainer.append(hourColumnContainer)
+slotContainer.append(textColumnContainer)
+slotContainer.append(saveButtonColumnContainer)
+hourColumnContainer.append(hourCard)
+textColumnContainer.append(textCard)
+saveButtonColumnContainer.append(saveButtonCard)
+textCard.append(textInput)
+if (locallyStoredDays !== null && locallyStoredDays[0] !== undefined) {
+  console.log('this is promising: ', locallyStoredDays[0].join(','))
+  textInput.val(locallyStoredDays[0].join(','))
+}
+
+hourCard.text(0)
+
+for (let i = 1; i < 24; i++) {
+  slotContainer.before(slotContainer.clone())
+  slotContainer.append(hourColumnContainer)
+  slotContainer.append(textColumnContainer)
+  slotContainer.append(saveButtonColumnContainer)
+  hourColumnContainer.append(hourCard)
+  textColumnContainer.append(textCard)
+  saveButtonColumnContainer.append(saveButtonCard)
+  textCard.append(textInput)
+  if (locallyStoredDays !== null && locallyStoredDays[i] !== undefined) {
+    textInput.val(locallyStoredDays[i].join(','))
+  } else {
+    textInput.val('')
+  }
+  
+  hourCard.text(`${i}`)
+}
+
+var hourCards = $('.hour')
+
+console.log('hourCards', hourCards)
+
+hourCards.each(hour => {
+  var currentCardHours = hourCards[hour].text
+
+  console.log("here we go again: ", $(hourCards[hour]).parent().siblings().find('.text'))
+
+  // console.log("asjk;ljlks333434: ", $(hourCards[hour]).parent().siblings()[0])
+
+  if (currentTimeHour > parseInt(currentCardHours)) {
+    $(hourCards[hour]).parent().siblings().find('.text').addClass('past')
+  } else if (currentTimeHour < parseInt(currentCardHours)) {
+    $(hourCards[hour]).parent().siblings().find('.text').addClass('future')
+  } else if (currentTimeHour === parseInt(currentCardHours)) {
+    $(hourCards[hour]).parent().siblings().find('.text').addClass('present')
+  }
+})
+
+
+function storeTask() {
+  console.log()
+}
+
+timeBlockContainer.bind('click', function(e) {
+  e.preventDefault()
+  if (e.target.id === 'saveBtn') {
+    var taskHour = $(e.target).parent().siblings()[0].childNodes[0].textContent
+    var taskText = $(e.target).parent().siblings()[1].childNodes[0].childNodes[0].value
+    var dayTasks = JSON.parse(localStorage.getItem("dayTasks")) || {}
+
+    dayTasks[taskHour] = taskText.split(',')
+
+    localStorage.setItem("dayTasks", JSON.stringify(dayTasks))
+    console.log("is dayTasks undefined atm? ", dayTasks[taskHour])
+  }
+})
